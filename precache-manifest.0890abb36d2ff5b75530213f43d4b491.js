9752cb5945591b16a68de4110faebc9d8de324ae
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-05 15:31:42 +0100

    Updates

---
+++ precache-manifest.0890abb36d2ff5b75530213f43d4b491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f74c082c523cd2aa859afd9ac5d24fd",
+    "url": "/complexCrediCard/index.html"
+  },
+  {
+    "revision": "b3927cfb6c99b364ce3d",
+    "url": "/complexCrediCard/static/css/main.52475edc.chunk.css"
+  },
+  {
+    "revision": "4f8ad38ce0e270d0ff64",
+    "url": "/complexCrediCard/static/js/2.06448520.chunk.js"
+  },
+  {
+    "revision": "b3927cfb6c99b364ce3d",
+    "url": "/complexCrediCard/static/js/main.dd9a578f.chunk.js"
+  },
+  {
+    "revision": "60d62ed7e5f2450a7f03",
+    "url": "/complexCrediCard/static/js/runtime-main.aeedc1e4.js"
+  }
+]);
